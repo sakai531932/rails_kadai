42b9f01030b8f65ac692a08c84f95c5fd8a563ec
”酒井” <sakai531932@gmail.com>
2020-10-13 03:53:36 +0000

    rails05kadai

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new
     
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(session_params)
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
@@ -12,9 +12,22 @@ class SessionsController < ApplicationController
     end
   end
 
+　def destroy
+    log_out
+    redirect_to root_url, info: 'ログアウトしました'
+  end
+  
   private
+  def session_params
+  params.require(:session).permit(:name, :email, :password)
+end
   def log_in(user)
     session[:user_id] = user.id
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 　end  
 end
