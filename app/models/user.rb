6f10d575531bf1e56d939188048ddfe8746c47bb
”酒井” <sakai531932@gmail.com>
2020-10-13 01:54:29 +0000

    rails04kadai

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :name, length: { maximum: 15 }
-  validates :email, presence: true
-  validates :email,format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-  validates :password, length: { in: 8..32 } 
+  RULE =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: {with: RULE}
+  validates :password, length: { in: 8..32 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :password_confirmation, length: { in: 8..32 }   
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
    
    #VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
    ##validates :password,  format: { with: VALID_PASSWORD_REGEX}
